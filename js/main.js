edde8ec6c3d8c58dac4513e98d44a5706feb61ae
mahedee007 <mahedee007@gmail.com>
2021-09-02 22:27:06 +0600

    https-problem-solved

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const searchBook = () => {
         alert('please enter a book name')
         
     }else{
-        const url = `http://openlibrary.org/search.json?q=${searchText}`
+        const url = `https://openlibrary.org/search.json?q=${searchText}`
         fetch(url)
         .then(res => res.json())
         .then(data => searchResult(data))
